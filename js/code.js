764f83f817d59358ba281f5e4f645b4d82d01bd2
enxel <26006596+enxel@users.noreply.github.com>
2025-07-08 18:08:29 -0600

    Add first js code

---
+++ js/code.js
@@ -0,0 +1,19 @@
+let name = "John";
+let surname = "Doe";
+
+console.log(name);
+console.log(surname);
+
+let age = 11;
+console.log(age);
+
+age = 54;
+
+console.log(age);
+
+const pi = 3.14;
+//pi = 10;
+
+console.log(pi);
+
+console.log((3+2)-76*(1+1));
